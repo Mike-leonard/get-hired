86dd7304b4fa8691bdeec66b7353f0641c7fcb98
leonard <mortuza.7@gmail.com>
2023-04-10 02:39:09 +0600

    home banner added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ActiveLink from './ActiveLink';
+import HomeBanner from './banner/HomeBanner';
 
 const Header = () => {
     return (
-        <header className='flex justify-between items-center'>
-            <div><h2 className='text-3xl cursor-pointer'>GetHired</h2></div>
+        <header className='bg-[#DBCFC7] p-5'>
+            <div className='flex justify-between items-center'>
+            <h2 className='text-3xl cursor-pointer'>GetHired</h2>
+           
             <nav>
                 <ul className='flex gap-10'>
                     <li><ActiveLink to="/">Home</ActiveLink></li>
@@ -16,6 +19,8 @@ const Header = () => {
 
             <button className='bg-gradient-to-r from-sky-500 to-indigo-500 text-white
             p-3 rounded-lg font-semibold'>Start Applying</button>
+            </div>
+            <HomeBanner />
         </header>
     );
 };
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div className='mx-5 my-2'>
+        <div className=''>
             <Header />
             <Outlet />
         </div>
