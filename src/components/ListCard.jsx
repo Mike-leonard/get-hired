34816bd083c60820a5357032ad91a9573e405ac7
leonard <mortuza.7@gmail.com>
2023-04-10 12:47:22 +0600

    category list design complete

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -4,13 +4,13 @@ const ListCard = ({item}) => {
     const { icon, title , text} = item;
     console.log(icon)
     return (
-        <div className="card">
-            <div className="card-icon"> 
-                <img src={icon} alt="" />
+        <div className="card rounded-md bg-[#DBCFC7] px-10 py-7">
+            <div className="my-5 "> 
+                <img className='h-20 bg-[#cac6c3] rounded-lg p-1' src={icon} alt="" />
             </div>
             <div className="card-content">
-                <h3 className="card-title">{title}</h3>
-                <p className="card-text"> { text}</p>
+                <h3 className="my-2 text-xl font-semibold">{title}</h3>
+                <p className="text-[#622e2e] "> { text}</p>
             </div>
         </div>
     );
--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -20,9 +20,12 @@ const Index = () => {
         <div className='p-5 my-8'>
             <h2 className='text-4xl font-bold text-center'>Job Category List</h2>
             <p className='text-center mt-4 text-[#757575]'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            {
-                data.map(item => <ListCard item={item} key={item.id} />)
-            }
+
+            <div className='flex gap-10 my-8 justify-center'>
+                {
+                    data.map(item => <ListCard item={item} key={item.id} />)
+                }
+            </div>
         </div>
     );
 };
