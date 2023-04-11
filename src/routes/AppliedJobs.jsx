dbf0e3134ea97a1a664c1a6169372328eb3afd26
leonard <mortuza.7@gmail.com>
2023-04-11 18:58:42 +0600

    filtering jobs done

--- src/routes/AppliedJobs.jsx
+++ src/routes/AppliedJobs.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppliedCard from '../components/AppliedCard';
 import { useLoaderData } from 'react-router-dom';
 
 const AppliedJobs = () => {
 
-    const { savedJobList } = useLoaderData()
-    console.log(savedJobList)
+    /* state for filtering Applied jobs */
+    const [savedJob, setSavedJob ] = useState([])
+    let { savedJobList } = useLoaderData()
+
+    useEffect( () => {
+        setSavedJob(savedJobList)
+    }, [])
+
+    /* filter Implementation */
+    const handleChange = (event) => {
+        const selectedValue = event.target.value;
+        if (selectedValue) {
+            const jobFilter = savedJobList.filter(job => job.jobType === selectedValue)
+            setSavedJob(jobFilter)
+        }
+      
+    }
+
     return (
         <div className='p-5 my-8'>
             <div className='py-2 mb-2 flex justify-end w-8/12 mx-auto'>
-                <select id="pet-select" className='p-2 rounded-sm'>
+                <select id="pet-select" className='p-2 rounded-sm' onChange={handleChange}>
                     <option value="">Filter By</option>
-                    <option value="remote">Remote</option>
-                    <option value="onsite">Onsite</option>
+                    <option value="Remote">Remote</option>
+                    <option value="Onsite">Onsite</option>
                 </select>
             </div>
 
             {
-                savedJobList.map(singleJob => <AppliedCard singleJob={singleJob} key={singleJob.id} />)
+                savedJob.map(singleJob => <AppliedCard singleJob={singleJob} key={singleJob.id} />)
             }
         </div>
     );
