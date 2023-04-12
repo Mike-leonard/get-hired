4349174ea39bd62a8f152726ff7bcfe8c8371a6c
leonard <mortuza.7@gmail.com>
2023-04-12 13:26:18 +0600

    404 bit design

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,7 +5,8 @@ export default function ErrorPage() {
     console.error(error);
 
     return (
-        <div id="error-page">
+        <div id="error-page" className="flex flex-col items-center justify-center h-screen">
+            <img className="w-1/5" src="https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg" alt="" />
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
